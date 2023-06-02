76493cf64844250aa96dc6c073e1f7bbb2eafe9c
Tcadle434 <thomascadle434@gmail.com>
2023-06-02 10:56:26 -0600

    first implementation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,43 @@
-import type { NextPage } from 'next'
-import { useState } from 'react'
-import styles from '../styles/Home.module.css'
-import AddressForm from '../components/AddressForm'
-import * as Web3 from '@solana/web3.js'
+import type { NextPage } from 'next';
+import { useState } from 'react';
+import styles from '../styles/Home.module.css';
+import AddressForm from '../components/AddressForm';
+import * as web3 from '@solana/web3.js';
 
 const Home: NextPage = () => {
-  const [balance, setBalance] = useState(0)
-  const [address, setAddress] = useState('')
+  const [balance, setBalance] = useState(0);
+  const [address, setAddress] = useState('');
+  const [executable, setExecutable] = useState(false);
 
   const addressSubmittedHandler = (address: string) => {
     try {
-      setAddress(address)
-      const key = new Web3.PublicKey(address)
-      const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
-      connection.getBalance(key).then(balance => {
-        setBalance(balance / Web3.LAMPORTS_PER_SOL)
-      })
+      setAddress(address);
+      const key = new web3.PublicKey(address);
+      const connection = new web3.Connection(web3.clusterApiUrl('devnet'));
+      connection.getBalance(key).then((balance) => {
+        setBalance(balance / web3.LAMPORTS_PER_SOL);
+      });
+      connection.getAccountInfo(key).then((info) => {
+        setExecutable(info?.executable || false);
+      });
     } catch (error) {
-      setAddress('')
-      setBalance(0)
-      alert(error)
+      setAddress('');
+      setBalance(0);
+      alert(error);
     }
-  }
+  };
 
   return (
     <div className={styles.App}>
       <header className={styles.AppHeader}>
-        <p>
-          Start Your Solana Journey
-        </p>
+        <p>Start Your Solana Journey</p>
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Is executable? ${executable}`}</p>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
